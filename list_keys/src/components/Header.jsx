39950321c06d8fe881a916951fc4d00d8ece881e
SoNomad <nurmuhammad16203@gmail.com>
2022-10-13 16:48:35 +0300

    added alt for cart ico

--- list_keys/src/components/Header.jsx
+++ list_keys/src/components/Header.jsx
@@ -5,7 +5,7 @@ export function Header () {
                 <h3>Intocode Coding Shopcamp</h3>
             </div>
             <div>
-                <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" width={'25px'}/>
+                <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="cart" width={'25px'}/>
             </div>
         </header>
     )
